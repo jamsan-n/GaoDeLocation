b1bc2bcfb9ff0946ed67635a24f866e005ce50f6
njs <njs>
2020-05-20 11:28:23 +0800

    补充js

--- www/GaoDeLocation.js
+++ www/GaoDeLocation.js
@@ -2,8 +2,28 @@ var exec = require('cordova/exec');
 
 var GaoDe = {
     getCurrentPosition:function (successFn,errorFn) {
-        exec(successFn,errorFn,'GaoDeLocation','getCurrentPosition',[]);
-    }
+		exec(successFn,errorFn,'GaoDeLocation','getCurrentPosition',[]);
+	},
+	startWatchPosition:function (successFn,errorFn) {
+		exec(successFn,errorFn,'GaoDeLocation','startWatchPosition',[]);
+	},
+	stopWatchPosition:function (successFn,errorFn) {
+		exec(successFn,errorFn,'GaoDeLocation','stopWatchPosition',[]);
+	},
+	config:function (successFn,errorFn, data){
+		exec(successFn,errorFn,'GaoDeLocation','config',[data]);
+	},
+	call: function(func, data){
+		if (typeof(data) == 'undefined')
+			data = null;
+		return new Promise((resolve, reject)=>{
+			this[func](res => {
+				resolve(res);
+			}, err => {
+				reject(err);
+			}, data);
+		});
+	},
 };
 
 module.exports = GaoDe;
